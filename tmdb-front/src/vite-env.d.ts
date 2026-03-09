3d94dc10e160f9e2728e7d3727fadcb33aa014e5
= <baltaz.samuel@gmail.com>
2026-03-08 23:15:28 -0300

    feat: add TypeScript definitions for Vite environment variables

---
+++ tmdb-front/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TMDB_API_BASE_URL: string
+  readonly VITE_TMDB_API_KEY: string
+  readonly VITE_TMDB_BEARER_TOKEN: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
